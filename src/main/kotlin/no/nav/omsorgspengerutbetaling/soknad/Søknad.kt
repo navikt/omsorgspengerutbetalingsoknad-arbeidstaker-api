7e7e4658ea4eb8e7de6e662e69484e0c7cd87c6e
Sander Opperud Odden <sander.opperud.odden@nav.no>
2021-03-15 15:19:12 +0100

    Legger til todo at hjemmePgaStengtBhgSkole og hjemmePgaSmittevernhensyn skal fjernes. Beholdes nå slik at vi ikke mister data i overgangen

--- src/main/kotlin/no/nav/omsorgspengerutbetaling/soknad/Søknad.kt
+++ src/main/kotlin/no/nav/omsorgspengerutbetaling/soknad/Søknad.kt
@@ -16,8 +16,8 @@ data class Søknad(
     val erFrilanser: JaNei = JaNei.Nei,
     val fosterbarn: List<FosterBarn>? = listOf(),
     val vedlegg: List<URL>,
-    val hjemmePgaSmittevernhensyn: Boolean,
-    val hjemmePgaStengtBhgSkole: Boolean? = null, // TODO låses til JaNei etter lansering.
+    val hjemmePgaSmittevernhensyn: Boolean? = null, //TODO 15.03.2021 - Fjernes når frontend er prodsatt
+    val hjemmePgaStengtBhgSkole: Boolean? = null, //TODO 15.03.2021 - Fjernes når frontend er prodsatt
     val barn: List<Barn> = listOf()
 ) {
 
