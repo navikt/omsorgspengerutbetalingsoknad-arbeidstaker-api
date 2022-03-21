022222e7903c2ab8ee907f1abd1290cad050f309
SanderOpperud <60642441+SanderOpperud@users.noreply.github.com>
2022-03-21 11:44:33 +0100

    Oppdatere actions, pakker og Redis til siste stable versjon. (#116)
    * Oppdaterer actions og pakker.
    
    * Closes #114
    * Closes #111
    * Closes #109
    * Closes #108
    * Closes #107
    * Closes #103
    
    * Dytter opp redis til siste stable variant. 6.2.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val dusseldorfKtorVersion = "3.1.6.7-f942a29"
+val dusseldorfKtorVersion = "3.1.6.7-05da1a0"
 val ktorVersion = ext.get("ktorVersion").toString()
 val mainClass = "no.nav.omsorgspengerutbetaling.AppKt"
-val lettuceVersion = "5.2.2.RELEASE"
-val k9FormatVersion = "5.7.2"
+val lettuceVersion = "6.1.8.RELEASE"
+val k9FormatVersion = "5.8.3"
 val fuelVersion = "2.3.1"
 val kafkaEmbeddedEnvVersion = ext.get("kafkaEmbeddedEnvVersion").toString()
 val kafkaVersion = ext.get("kafkaVersion").toString() // Alligned med version fra kafka-embedded-env
@@ -17,7 +17,7 @@ plugins {
 
 buildscript {
     // Henter ut diverse dependency versjoner, i.e. ktorVersion.
-    apply("https://raw.githubusercontent.com/navikt/dusseldorf-ktor/f942a29fd1007215e0254b7d1a83fbc7a3fe4c21/gradle/dusseldorf-ktor.gradle.kts")
+    apply("https://raw.githubusercontent.com/navikt/dusseldorf-ktor/05da1a09b4cad3aef489f934078ac8afafe155ae/gradle/dusseldorf-ktor.gradle.kts")
 }
 
 dependencies {
@@ -47,16 +47,16 @@ dependencies {
     implementation("org.glassfish:jakarta.el:3.0.4")
 
     // Test
-    testImplementation("com.github.fppt:jedis-mock:1.0.0")
+    testImplementation("com.github.fppt:jedis-mock:1.0.1")
     testImplementation("no.nav.helse:dusseldorf-test-support:$dusseldorfKtorVersion")
     testImplementation("io.ktor:ktor-server-test-host:$ktorVersion") {
         exclude(group = "org.eclipse.jetty")
     }
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation ("org.skyscreamer:jsonassert:1.5.0")
-    testImplementation("org.awaitility:awaitility-kotlin:4.1.1")
+    testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
     testImplementation("no.nav:kafka-embedded-env:$kafkaEmbeddedEnvVersion")
-    testImplementation("io.mockk:mockk:1.12.2")
+    testImplementation("io.mockk:mockk:1.12.3")
 }
 
 repositories {
